9ee52d8250f34dc4bb5b5e2c07fec8c5553ae94e
daniel <mr.daniels.2025@gmail.com>
2022-06-08 16:36:53 -0600

    Add array method

--- friends.js
+++ friends.js
@@ -3,3 +3,6 @@ var arrays = ["string 1", "string 2", "string 3", "string 4", "string 5"];
 console.log(arrays[2]);
 console.log(arrays[4]);
 console.log(arrays[0]);
+
+var places = ["Costa Rica", "Pikes Peak", "Mount Fuji", "Volcano Arenal", "Colorado"];
+console.log(places.slice(0, 1));
